49bfa910d6648d85087d51194c802ec9795b27c6
ZeAftonFamily <81208851+ZeAftonFamily@users.noreply.github.com>
2021-03-27 19:50:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+menu_list_array= ["Chicken Tandoori",
+"Veggie Mania",
+"Pepperoni",
+"Veg Supreem Pizza",
+"Paneer Tikka Pizza",
+]
+
+function getmenu(){
+    var htmldata
+    htmldata="<ol class=menulist"
+    menu_list_array.sort()
+    for(var i=0;i<menu_list_array.length;i++)
+    htmldata=htmldata +'<li>' + menu_list_array[i] + '</li>'
+}
+    
+    htmldata=htmldata"</ol>"
+    document. getElementById("display_menu").innerhtml  = htmldata;
+
+
+}
+
+
+function, add_ltem()(
+    var htmldata;
+    var item= document.getElementById("add_item").value
+menu_list_array.push(item);
+    menu_list_array.sort(); 
+    htmldata="<section class="cards" >"
+    for(var i=0;i<menu_list_array.length;i++)
+    htmldata + "<div class="cards"" + "<img src"images/pizzaImage.png"/>" +menu_list_array[i] "</div>"
+
+
+
+
+
+}
+
+htmldata=htmldata"</section>"
+document. getElementById("display_menu").innerhtml  = htmldata;
